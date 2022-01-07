04f5ca53579238dba592e0c90ac5e67d470d3ef1
Nikolay Nikolaenko <niko.dr@gmail.com>
2022-01-07 14:19:36 +0200

    adds methods

--- StringMethods.py
+++ StringMethods.py
@@ -29,3 +29,59 @@ print(f"time.split(\":\") = {time.split(':')}")
 str4 = "  time  "
 print(f"str4.strip() = {str4.strip()}")
 print(" - ".join(["a", "b", "c"]))
+
+# моё приложение
+str = "Hello, world!"
+print(f"str.casefold() = {str.casefold()}") # переводит в нижний регистр
+print(f"str.capitalize() = {str.capitalize()}") # первая буква в верхний регистр
+print(f"str.center(20) = {str.center(20)}") # выравнивание по центру
+print(f"str.center(40) = {str.center(40, '.')}") # выравнивание по центру
+print(f"str.center(70) = {str.center(70, '*')}") # выравнивание по центру
+str = "test\tstring\tend"
+print(str)
+print(f"str.expandtabs() = {str.expandtabs()}") # заменяет табуляции на пробелы
+# string encode
+str = "test for encode!"
+print("str.encode() = ", str.encode("ascii", "ignore"))
+print("str.encode() = ", str.encode("ISO-8859-5", "ignore"))
+
+# string decode
+str = b"test for decode!"
+print("str.decode() = ", str.decode("ascii", "ignore"))
+
+# string isalnum()
+str = "testSstring12"
+print(f"str.isalnum() = {str.isalnum()}")
+str = "test string!"
+print(f"str.isalnum() = {str.isalnum()}")
+
+# string isidentifier()
+str = "testSstring12"
+print(f"str.isidentifier() = {str.isidentifier()}")
+
+# string isnumeric()
+str = "1234"
+print(f"str.isnumeric() = {str.isnumeric()}")
+str = "1234.5"
+print(f"str.isnumeric() = {str.isnumeric()}")
+str = "1234.5.6"
+print(f"str.isnumeric() = {str.isnumeric()}")
+# swap case
+str = "test string!"
+print(f"str.swapcase() = {str.swapcase()}")
+str = "TesTinG StRiNg!"
+print(f"str.swapcase() = {str.swapcase()}")
+
+# partition
+str = "test string!"
+a = str.partition(" ")
+print(type(a))
+print(f"str.partition(\"string\") = {str.partition('string')}")
+
+# maketrans
+str = "test string!"
+print(f"str.maketrans() = {str.maketrans('t', 'T')}")
+
+# zfill
+str = "test string!"
+print(f"str.zfill(10) = {str.zfill(20)}")
